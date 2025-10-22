b666f9c638b7e233d8d02d84e8a35b6987000d4f
Pandemolyum <77362203+Pandemolyum@users.noreply.github.com>
2025-10-22 16:12:57 -0400

    Add styling

--- src/contact.js
+++ src/contact.js
@@ -1,36 +1,39 @@
+import smallPolloLogo from "./assets/img/Los_Pollos_Hermanos_logo.png";
+
 function createContactTab(contentDiv) {
+    const contactDiv = document.createElement("div");
     const h1 = document.createElement("h1");
-    const h3 = document.createElement("h3");
-    const p1 = document.createElement("p");
+    const h4 = document.createElement("h4");
     const span1 = document.createElement("span");
     const span2 = document.createElement("span");
-    const p2 = document.createElement("p");
     const span3 = document.createElement("span");
     const span4 = document.createElement("span");
-    const p3 = document.createElement("p");
     const span5 = document.createElement("span");
     const span6 = document.createElement("span");
+    const smallImg = document.createElement("img");
     
+    contactDiv.classList.add('contact');
+
     h1.textContent = "Contact Us";
-    h3.textContent = "We are here to help! Let us know how we can best serve you.";
+    h4.textContent = "We are here to help! Let us know how we can best serve you.";
     span1.textContent = "Phone number: ";
     span2.textContent = "555-555-5555";
     span3.textContent = "Email: ";
     span4.textContent = "contact@lospolloshermanos.com";
     span5.textContent = "Address: ";
     span6.textContent = "123 Chicken Rd, BB";
+    smallImg.src = smallPolloLogo;
     
     contentDiv.appendChild(h1);
-    contentDiv.appendChild(h3);
-    p1.appendChild(span1);
-    p1.appendChild(span2);
-    contentDiv.appendChild(p1);
-    p2.appendChild(span3);
-    p2.appendChild(span4);
-    contentDiv.appendChild(p2);
-    p3.appendChild(span5);
-    p3.appendChild(span6);
-    contentDiv.appendChild(p3);
+    contentDiv.appendChild(h4);
+    contactDiv.appendChild(span1);
+    contactDiv.appendChild(span2);
+    contactDiv.appendChild(span3);
+    contactDiv.appendChild(span4);
+    contactDiv.appendChild(span5);
+    contactDiv.appendChild(span6);
+    contentDiv.appendChild(contactDiv);
+    contentDiv.appendChild(smallImg);
 }
 
 export {createContactTab};
